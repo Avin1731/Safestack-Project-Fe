7a52f4e9c234faf0eb5fd4994b7dcc70bb6673a0
Shiny1412 <les.017ciers@gmail.com>
2025-12-23 01:00:09 +0700

    Fase 2: Setup Axios Interceptor, Google Provider, and Env Frontend

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,18 @@ import App from './App.jsx'
 import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    {/* Google Provider harus membungkus App */}
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </GoogleOAuthProvider>
   </React.StrictMode>,
 )
